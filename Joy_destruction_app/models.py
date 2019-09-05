dbd83ba9ab58ef365c397beb5214703fe3d0fd17
rafaelcharris <rafaelcharris92@gmail.com>
2019-09-05 14:27:15 -0500

    agregar destructon game

---
+++ Joy_destruction_app/models.py
@@ -0,0 +1,36 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+#Estew codigo funciona. Solo falta cuadrar la diapositiva de resultados.
+#escribir mejor las instrucciones
+author = 'Rafael'
+
+doc = """
+Joy of destruction
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'Joy_destruction_app'
+    players_per_group = 2
+    num_rounds = 1
+
+    #constantes iniciales
+    endowment = c(10000)
+    destruction_factor = 1/2
+    penalty =c(1000)
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    #Acá el usuario escribe si decide o no destruir
+    destroy = models.BooleanField(
+        label = "¿Quiere destruir la mitad del dinero del otro jugador?"
+    )
---
+++ Joy_destruction_app/pages.py
@@ -0,0 +1,48 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+class instrucciones(Page):
+    form_model = 'player'
+
+class MyPage(Page):
+    form_model = 'player'
+    form_fields = ['destroy']
+
+class Results(Page):
+    #Estos son espacios que el jugador puede llenar
+    form_model = 'player'
+    form_fields = ['destroy']
+
+    #Esta funcion trae al template
+    def vars_for_template(self):
+        return dict(
+            penalty=Constants.penalty
+            )
+
+class ResultsWaitPage(WaitPage):
+    #Hacer calculos del payoff
+    def after_all_players_arrive(self):
+        #Deme el grupo que participó en cada ronda
+        group = self.group
+
+        #de este grupo, clasifiqué toma los jugadores por el "tipo" que son dentro del grupo
+        p1 = group.get_player_by_id(1)
+        p2 = group.get_player_by_id(2)
+
+        #Mejor función de pago.
+        #para no hacer ifs, es mejor usar multiplicación que sea cero si el jugador no tomó la decisión de destruir
+        #y así mismo que se active la penalty si yo decidí destruir.
+        p1.payoff = Constants.endowment*(1 - Constants.destruction_factor * int(p2.destroy)) - \
+                    (int(p1.destroy) * Constants.penalty)
+        p2.payoff = Constants.endowment * (1 - Constants.destruction_factor * int(p1.destroy)) - \
+                    (int(p2.destroy) * Constants.penalty)
+
+
+page_sequence = [
+    instrucciones,
+    MyPage,
+    ResultsWaitPage,
+    Results
+]
+
