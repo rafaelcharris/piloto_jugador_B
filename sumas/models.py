e22f4b64e953561bd4683da76c55b90a4c47ae3c
rafaelcharris <rafaelcharris92@gmail.com>
2019-09-06 09:52:11 -0500

    agregar juego de sumas de cinco numeros. falta programar pagos y que sea por tiempo

--- sumas/models.py
+++ sumas/models.py
@@ -2,12 +2,12 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
+import random as rm
 
-
-author = 'Your name here'
+author = 'Rafael'
 
 doc = """
-Your app description
+Juego sumar cinco números de dos dígitos
 """
 
 
@@ -16,6 +16,10 @@ class Constants(BaseConstants):
     players_per_group = None
     num_rounds = 1
 
+    #generate one problem
+    problem = [rm.randint(10,99) for x in range(5)]
+    solution = sum(problem)
+    print(solution)
 
 class Subsession(BaseSubsession):
     pass
@@ -26,4 +30,13 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    #Uso esto para poder imprimir el problem como una list al player
+    player_problem = map(str, Constants.problem)
+
+    p_solution = models.IntegerField(label = ("+".join(player_problem)))
+
+    #Esto es para guardar en la base de datos si la respuesta fue correcta o no (de hecho creo que me toca es guaradare l numero de corrects)
+    is_correct = models.BooleanField()
+
+    def check_answer(self):
+        self.is_correct = (self.p_solution == Constants.solution)
--- sumas/pages.py
+++ sumas/pages.py
@@ -4,21 +4,19 @@ from .models import Constants
 
 
 class MyPage(Page):
-    pass
-
-
-class ResultsWaitPage(WaitPage):
-
-    def after_all_players_arrive(self):
-        pass
-
+    form_model = 'player'
+    form_fields = ['p_solution']
 
 class Results(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['p_solution', 'is_correct']
+
+    def vars_for_template(self):
+        self.player.check_answer()
+
 
 
 page_sequence = [
     MyPage,
-    ResultsWaitPage,
     Results
 ]
