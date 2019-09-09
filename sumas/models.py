1ffc9201cf91bd1ae2b3986fea1bc97b9427ee0a
rafaelcharris <rafaelcharris92@gmail.com>
2019-09-09 18:30:35 -0500

    no funcionagit add sumas/models.py . intento de mover problem generation a subsession

--- sumas/models.py
+++ sumas/models.py
@@ -19,9 +19,8 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     # generate one problem
     def creating_session(self):
-        for player in self.get_players():
-                player.problem = [rm.randint(10,99) for x in range(5)]
-                solution = sum(player.problem)
+            self.problem = [rm.randint(10,99) for x in range(5)]
+            self.solution = sum(self.problem)
 
 
 class Group(BaseGroup):
@@ -32,11 +31,13 @@ class Player(BasePlayer):
     #Uso esto para poder imprimir el problem como una list al player
     def get_problems(self):
         self.subsession.creating_sessions(self)
-        player_problem = map(str, self.problem)
-        p_solution = models.IntegerField(label = ("+".join(player_problem)))
+        player_problem = map(str, self.subsession.creating_session().problem)
+        return player_problem
+
+    p_solution = models.IntegerField(label = ("+".join(get_problems.player_problem)))
 
     #Esto es para guardar en la base de datos si la respuesta fue correcta o no (de hecho creo que me toca es guaradare l numero de corrects)
     is_correct = models.BooleanField()
 
     def check_answer(self):
-        self.is_correct = (self.p_solution == self.subsession.solution)
+        self.is_correct = (self.p_solution == self.subsession.creating_sessions.solution)
