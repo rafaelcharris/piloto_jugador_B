3cc4a9762afd96206ec5d19723a8e95db1609d05
rafaelcharris <rafaelcharris92@gmail.com>
2019-09-09 18:04:50 -0500

    agregar codigo para generar problemas y chequear respuestas de cada jugador

--- sumas/models.py
+++ sumas/models.py
@@ -19,8 +19,9 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     # generate one problem
     def creating_session(self):
-        problem = [rm.randint(10,99) for x in range(5)]
-        solution = sum(problem)
+        for player in self.get_players():
+                player.problem = [rm.randint(10,99) for x in range(5)]
+                solution = sum(player.problem)
 
 
 class Group(BaseGroup):
@@ -29,12 +30,13 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     #Uso esto para poder imprimir el problem como una list al player
-    player_problem = map(str, Constants.problem)
-
-    p_solution = models.IntegerField(label = ("+".join(player_problem)))
+    def get_problems(self):
+        self.subsession.creating_sessions(self)
+        player_problem = map(str, self.problem)
+        p_solution = models.IntegerField(label = ("+".join(player_problem)))
 
     #Esto es para guardar en la base de datos si la respuesta fue correcta o no (de hecho creo que me toca es guaradare l numero de corrects)
     is_correct = models.BooleanField()
 
     def check_answer(self):
-        self.is_correct = (self.p_solution == Constants.solution)
+        self.is_correct = (self.p_solution == self.subsession.solution)
