b7819a72f916a9ea099c1c8aa3915fb5ad2e2212
rafaelcharris <rafaelcharris92@gmail.com>
2019-09-23 21:19:04 -0500

    borrar páginas extra de pages

--- appr_game/pages.py
+++ appr_game/pages.py
@@ -2,34 +2,15 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-class pregunta(Page):
-    form_fields = ['lotto']
-    form_model = 'player'
-
-    def vars_for_template(self):
-        return dict(
-        endowment=Constants.endowment,
-        )
-
-class Results(Page):
-    form_fields = ['lotto']
-    form_model = 'player'
-
-    def vars_for_template(self):
-        payoff = self.player.set_payoffs()
-
 class appropriation(Page):
     form_fields = ['appr']
     form_model = 'player'
-
 
 class Final(Page):
     form_fields = ['appr']
     form_model = 'player'
 
 page_sequence = [
-    pregunta,
-    Results,
     appropriation,
     Final
 ]
