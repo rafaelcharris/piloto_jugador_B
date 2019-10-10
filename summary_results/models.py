95396074491ed7b123fccacea4205293739c3b3b
rafaelcharris <rafaelcharris92@gmail.com>
2019-10-09 23:10:16 -0500

    agregar print para verificar pago. La tabla funciona

--- summary_results/models.py
+++ summary_results/models.py
@@ -57,10 +57,9 @@ class Player(BasePlayer):
         self.summary_belief = self.participant.vars.get('belief')
         self.summary_belief_was_correct = self.participant.vars.get('belief_was_correct')
         self.summary_jod_payoff = self.participant.vars.get('jod_payoff')
-        print("payoff del jugador = " + str(self.participant.vars.get('jod_payoff')))
 
         self.summary_FINAL_payoff = self.participant.vars.get('addition_final_payoff') + 1000*self.participant.vars.get('jod_payoff') #Revisar el pago
-
+        print('el pago final es: ' + str(self.summary_FINAL_payoff))
     def report_summary(self):
         self.participant.vars['FINAL_payoff'] = self.summary_FINAL_payoff
 
