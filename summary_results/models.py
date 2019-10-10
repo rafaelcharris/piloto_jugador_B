e4b0dde5686170fcec84f15050115671d3618c96
rafaelcharris <rafaelcharris92@gmail.com>
2019-10-09 21:52:19 -0500

    agrgar borrador de la tabla a mostrar a los participantes

--- summary_results/models.py
+++ summary_results/models.py
@@ -19,12 +19,29 @@ Your app description
 
 class Constants(BaseConstants):
     name_in_url = 'summary_results'
-    players_per_group = None
+    players_per_group = 2
     num_rounds = 1
 
 
 class Subsession(BaseSubsession):
-    pass
+
+    def vars_for_admin_report(self):
+        table_rows = []
+        for p in self.get_players():
+            row = p.participant.vars #quejesto?
+            row['participant_code'] = p.participant.code
+            row['consent_name'] = p.participant.vars.get('consent_name')
+            row['consent_id_number'] = p.participant.vars.get('consent_id_number')
+            row['addition_treatment'] = p.participant.vars.get('treatment')
+            row['addition_acc_was_correct'] = p.participant.vars.get('addition_acc_was_correct')
+            row['addition_acc_acc_payoff'] = p.participant.vars.get('addition_acc_acc_payoff')
+            row['addition_final_payoff'] = p.participant.vars.get('addition_final_payoff')
+            row[''] = p.participant.vars.get('')
+            row[''] = p.participant.vars.get('')
+            row[''] = p.participant.vars.get('')
+            row[''] = p.participant.vars.get('')
+            table_rows.append(row)
+        return {'table_rows': table_rows}
 
 
 class Group(BaseGroup):
