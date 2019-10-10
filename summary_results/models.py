456331a1eda3633b28890d6e218ba71cac1d70c8
rafaelcharris <rafaelcharris92@gmail.com>
2019-10-10 00:04:53 -0500

    agregar linea de pagos en puntos y en pesos en el informe de participantes

--- summary_results/models.py
+++ summary_results/models.py
@@ -41,7 +41,8 @@ class Player(BasePlayer):
     summary_joy_was_destroyed = models.BooleanField()
     summary_belief = models.BooleanField()
     summary_belief_was_correct = models.BooleanField()
-    summary_jod_payoff = models.CurrencyField()
+    summary_jod_payoff_points = models.CurrencyField()
+    summary_jod_payoff_cop = models.CurrencyField()
 
     #Final
     summary_FINAL_payoff = models.CurrencyField()
@@ -56,7 +57,8 @@ class Player(BasePlayer):
         self.summary_joy_was_destroyed = self.participant.vars.get('was_destroyed')
         self.summary_belief = self.participant.vars.get('belief')
         self.summary_belief_was_correct = self.participant.vars.get('belief_was_correct')
-        self.summary_jod_payoff = self.participant.vars.get('jod_payoff')
+        self.summary_jod_payoff_points = self.participant.vars.get('jod_payoff')
+        self.summary_jod_payoff_cop = 1000*self.participant.vars.get('jod_payoff')
 
         self.summary_FINAL_payoff = self.participant.vars.get('addition_final_payoff') + 1000*self.participant.vars.get('jod_payoff') #Revisar el pago
         print('el pago final es: ' + str(self.summary_FINAL_payoff))
